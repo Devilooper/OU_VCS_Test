abb3b66f6f47ce2ba223eb1f5a301d7e7deb87cc
Dawid Darłak <primsh@o2.pl>
2021-02-02 14:57:57 +0100

    metoda policz

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-
+int policz(int a,int b){
+    return a+b;
+}
 int main() {
-    printf("Hello, World!\n");
+    printf("Pierwszy branch 2+3=%d\n",policz(2,3));
     return 0;
 }
