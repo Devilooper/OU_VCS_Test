be014b6be6633f7f7b6701123b6e99fec494b8f7
Dawid Darłak <primsh@o2.pl>
2021-02-02 18:16:12 +0100

    zmiany przez SourceTree

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-
+int policz(int a,int b){
+    return (a+b) * (a-b);
+}
 int main() {
-    printf("Hello, World!\n");
+    printf("Policz3  =%d!\n",policz(2,3));
     return 0;
 }
